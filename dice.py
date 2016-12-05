6a3a284453221c8929df7e0d29fc1e75fd82247e
Iris Li <github@iris.li>
2016-12-04 20:48:33 -0800

    Delete a "random" block of code

--- dice.py
+++ dice.py
@@ -13,12 +13,6 @@ Types of dice:
     make_test_dice function.
 """
 
-from random import randint
-
-def make_fair_dice(sides):
-    """Return a die that returns 1 to SIDES with equal chance."""
-    assert type(sides) == int and sides >= 1, 'Illegal value for sides'
-    def dice():
         return randint(1,sides)
     return dice
 
